390bd74ad32bd891b2f93bd68bd1db092985806f
MosesBolt <umoses777@gmail.com>
2025-06-14 11:19:55 +0100

    Fix redirect with dynamic absolute URL

--- src/app/api/signup/route.js
+++ src/app/api/signup/route.js
@@ -11,13 +11,16 @@ export async function POST(req) {
     const data = await resend.emails.send({
       from: "onboarding@resend.dev",
       to: email,
-      subject: "Welcome to Geofonx from the CEO ( Moses Ugo)",
+      subject: "Welcome to Geofonx",
       html: `<p>You're officially on the chaos list. Expect builds. Expect noise.</p>`,
     });
 
     console.log("New sign up:", email);
     console.log("Resend response:", data);
-    return NextResponse.redirect("http://localhost:3000", { status: 303 });
+
+    const redirectURL = new URL("/", req.url);
+    return NextResponse.redirect(redirectURL, { status: 303 });
+
   } catch (err) {
     console.error("Email error:", err);
     return NextResponse.json(
